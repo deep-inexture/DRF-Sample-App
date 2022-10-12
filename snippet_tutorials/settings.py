95f7db6c13c96f666454b11fab6628fcf40d673f
deep <deep.inexture@gmail.com>
2022-10-12 14:52:47 +0530

    updated code - included swagger ui

--- snippet_tutorials/settings.py
+++ snippet_tutorials/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_yasg',
     'rest_framework',
+    'rest_framework_swagger',
     'snippets',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -108,7 +110,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- snippet_tutorials/urls.py
+++ snippet_tutorials/urls.py
@@ -19,11 +19,31 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
+# For Swagger UI Template
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import re_path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Welcome to the world of Snippets",
+        terms_of_service="https://www.jasonmars.org/2020/04/22/add-swagger-to-django-rest-api-quickly-4-mins-without-hiccups/",
+        contact=openapi.Contact(email="deep.inexture@gmail.com"),
+        license=openapi.License(name="Awesome IP"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is-staff']
+        fields = ['url', 'username', 'email', 'is_staff']
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -35,6 +55,11 @@ router = routers.DefaultRouter()
 router.register(f'users', UserViewSet)
 
 urlpatterns = [
+    # Route TemplateView to serve Swagger UI template.
+    #   * Provide `extra_context` with view name of `SchemaView`.
+    re_path(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     path('admin/', admin.site.urls),
 
     path('', include(router.urls)),
